bfadde1e4a1ec46678efaab3f4df29ed3320e345
Stefan Poindl <stefan@tohuwabohu.io>
2023-12-03 21:43:55 +0100

    day 03 wip

---
+++ src/main/scala/Day03.scala
@@ -0,0 +1,54 @@
+import util.input
+
+private lazy val lines = input("Day03.txt")
+
+private def part01(): Unit =
+  val result = lines.map(line => batch(lines, line)).map(triplet => {
+    val numberMatches = ("\\d+".r findAllIn triplet._2).toList
+
+    var activeLine = triplet._2
+
+    numberMatches.map(m => {
+      val number = m.toInt
+
+      val indexStart = activeLine.indexOf(m) - 1
+      val indexEnd = activeLine.indexOf(m) + m.length + 1
+
+      val above = if triplet._1 != null then findSymbols(indexStart, indexEnd, triplet._1) else 0
+      val adjacent = findSymbols(indexStart, indexEnd, triplet._2)
+      val below = if triplet._3 != null then findSymbols(indexStart, indexEnd, triplet._3) else 0
+
+      val sum = above + adjacent + below
+
+      println(s"found $sum for $number")
+
+      activeLine = activeLine.replaceFirst(m, if m.length == 3 then "..." else "..")
+
+      number * sum
+    }).sum
+  }).sum
+
+  println("Part 01")
+  println(result)
+
+private def findSymbols(startIndex: Int, endIndex: Int, line: String): Int =
+  val start = if (startIndex < 0) 0 else startIndex
+  val end = if (endIndex > line.length) line.length else endIndex
+
+  ("[^\\d.\\r\\n]".r findAllIn line.substring(start, end)).size
+
+private def batch(lines: LazyList[String], line: String) =
+  (getPreviousLine(lines, line), line, getNextLine(lines, line))
+
+private def getPreviousLine(lines: LazyList[String], line: String): String =
+  val previousIndex = lines.indexOf(line) - 1
+
+  if previousIndex < 0 then null else lines.apply(previousIndex)
+
+private def getNextLine(lines: LazyList[String], line: String): String =
+  val nextIndex = lines.indexOf(line) + 1
+
+  if nextIndex >= lines.length then null else lines.apply(nextIndex)
+
+@main def day03(): Unit =
+  part01()
