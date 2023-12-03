24362392b91e1675f212c947df1a9f8dabea9ad1
Stefan Poindl <stefan@tohuwabohu.io>
2023-12-03 13:07:31 +0100

    add description in Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 @main def aoc(): Unit =
   println("Advent of Code 2023 - Scala solutions by tohuwabohu.io")
   println(msg)
+  println("Please use sbt to run the different puzzle solutions.")
 
 def msg = "I was compiled by Scala 3. :)"
